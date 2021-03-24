5ac291cd698974001d83a8de960fe1126c9a9df5
MCFrank16 <boonatechnologies@gmail.com>
2021-03-24 09:54:38 +0200

    initializing the project

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
+
+// components
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+
+// Styles
+import { Wrapper } from './App.styles';
+
+function App() {
+  return (
+    <div className="App">
+      shopping cart
+    </div>
+  );
+}
+
+export default App;
